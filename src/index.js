dda68d10be5e66ad7cd5198c32cab46362655d69
Mitchel Myers <mitchelm@servantpc.com>
2019-07-23 15:29:04 -0400

    Updated to use @fullcalenar/core@^4

--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
 import React from 'react';
-import $ from "jquery";
-import fullCalendar from "fullcalendar";
-import moment from 'moment';
+import { Calendar } from "@fullcalendar/core";
+
 import FullcalendarObjectMapper from './fullcalendarObjectMapper';
 
-export default class FullCalendar extends React.Component{
-	constructor(){
-		super();
-		this.jq = $.noConflict();
-		this.fullcalendarObjectMapper = new FullcalendarObjectMapper();
-		this.root = null;
-		this.instance = null;
-		this.date = new Date();
-	}
+export default class FullCalendar extends React.PureComponent {
+    constructor (props) {
+        super(props);
+        this.fullcalendarObjectMapper = new FullcalendarObjectMapper();
+        this.rootEl = null;
+        this.calendar = null;
+        this.date = new Date();
+    }
 
-	componentDidMount(){
-		const objectMapperSettings = this.fullcalendarObjectMapper.getSettings(this.props);
-		this.instance = this.jq(`#${this.root}`).fullCalendar(objectMapperSettings);
-	}
+    componentDidMount() {
+        this.createCalendar();
+    }
 
-  	componentWillReceiveProps(nextProps){
-  		this.jq(`#${this.root}`).fullCalendar('destroy');
-  		const objectMapperSettings = this.fullcalendarObjectMapper.getSettings(nextProps);
-    	this.instance = this.jq(`#${this.root}`).fullCalendar(objectMapperSettings);
-  	}
+    componentDidUpdate() {
+        this.destroyCalendar();
+        this.createCalendar();
+    }
 
-	render(){
-		this.root = this.props.id || 'ID' + this.date.getTime(); 
-		return(
-			<div id={this.root}></div>
-		)
-	}
+    createCalendar() {
+        if (this.rootEl) {
+            const objectMapperSettings = this.fullcalendarObjectMapper.getSettings(this.props);
+            this.calendar = new Calendar(this.rootEl, objectMapperSettings);
+            this.calendar.render();
+        }
+    }
+
+    destroyCalendar() {
+        if (this.calendar) {
+            this.calendar.destroy();
+        }
+    }
+
+    render() {
+        return (
+            <div ref={(ref) => { this.rootEl = ref; }}></div>
+        )
+    }
 }
